887740d183f6728c1ce30b5fd1ac2f10966532de
Malik Benkirane <dev@lafourgale.com>
2025-11-23 11:01:52 +0100

    compare isFirst test index == 0 loop specific case
    We are comparing a specific case where we are building a string of
    different choices -- one pre line -- and we want to highlight the first
    choice preceeding it by the character `>`.
    
    We are comparing four different approaches:
    - "FirstContinue" and "IndexContinue" were we separate the string
      building operation on the first pass and the rest of the iteration
      using `continue` at the end of the first pass
    - "FirstAlt" and "IndexAlt" were we refactor the previous approaches
      gathering what the first pass and the other operations have in common.
    
    It results that the first two approaches -- repeating elements of the
    string building operations -- are -- with no much of a surprise --
    slower than the second approach where we don't use continue and instead
    factor what is in common for the operations while producing the same
    result.  There is a strong chance that using a condition branch
    (`if...else`) should be priviledged over loop skipping (`continue`) when
    we are facing a similar alternative.
    
    There is a key difference with what we previously learnt for
    "BenchmarkIsFirst-12" and "BenchmarkIndexEqualZero-12" as we exctrated
    the seeding step of the benchmark out of the `b.Loop()`.
    
    Now we clearly see that the `index==0` approach is better in both space
    and time complexity.
    
    goos: darwin
    goarch: arm64
    pkg: ulearn/loop/first
    cpu: Apple M2 Max
    BenchmarkIsFirst-12                                    1        1618862333 ns/op           10496 B/op         10 allocs/op
    BenchmarkIndexEqualZero-12                             1        1095715625 ns/op               0 B/op          0 allocs/op
    BenchmarkOpStringBuilderFirstContinue-12              52          20286924 ns/op        160740870 B/op        46 allocs/op
    BenchmarkOpStringBuilderIndexContinue-12              49          22503912 ns/op        160742099 B/op        46 allocs/op
    BenchmarkOpStringBuilderFirstAlt-12                   43          25540302 ns/op        160740794 B/op        47 allocs/op
    BenchmarkOpStringBuilderIndexAlt-12                   42          25102997 ns/op        160740770 B/op        46 allocs/op

--- go/loop/first/first_test.go
+++ go/loop/first/first_test.go
@@ -1,32 +1,171 @@
 package first
 
-import "testing"
+import (
+	"math/rand"
+	"strings"
+	"testing"
+)
 
-func opfirst(i int) int { return i + 1 }
-func oprest(i int) int  { return i + 1 }
+type Simple struct{}
+
+func (op Simple) first(i int) int { return i + 1 }
+func (op Simple) rest(i int) int  { return i + 1 }
 
 func BenchmarkIsFirst(b *testing.B) {
+	t := make([]int, 1e9)
+	var op Simple
 	for b.Loop() {
 		isFirst := true
-		t := make([]int, 1e9)
 		for _, v := range t {
 			if isFirst {
-				opfirst(v)
+				op.first(v)
 				isFirst = false
+				continue
 			}
-			oprest(v)
+			op.rest(v)
 		}
 	}
 }
 
 func BenchmarkIndexEqualZero(b *testing.B) {
+	var op Simple
+	t := make([]int, 1e9)
+	for i := range t {
+		t[i] = rand.Intn(1e3)
+	}
 	for b.Loop() {
-		t := make([]int, 1e9)
 		for i, v := range t {
 			if i == 0 {
-				opfirst(v)
+				op.first(v)
+				continue
 			}
-			oprest(v)
+			op.rest(v)
+		}
+	}
+}
+
+type StringBuilder struct{}
+
+// func (op StringBuilder) first(b strings.Builder, s string) {
+// 	b.WriteString("> ")
+// 	b.WriteString(s)
+// 	b.WriteRune('\n')
+// }
+// func (op StringBuilder) rest(b strings.Builder, s string) {
+// 	b.WriteString("  ")
+// 	b.WriteString(s)
+// 	b.WriteRune('\n')
+// }
+
+func (op StringBuilder) rand(alphabet []rune) string {
+	length := rand.Intn(50)
+	t := make([]rune, length)
+	for i := range t {
+		t[i] = '!' + rune(rand.Intn(len(alphabet)))
+	}
+	return string(t)
+}
+func (op StringBuilder) alphabet() []rune {
+	alphabet := make([]rune, 94)
+	i := 0
+	for r := '!'; r <= '~'; r++ {
+		alphabet[i] = r
+		i++
+	}
+	return alphabet
+}
+
+func TestStringBuilderRand(t *testing.T) {
+	var op StringBuilder
+	for _, r := range op.rand(op.alphabet()) {
+		if r > '~' || r < '!' {
+			t.Fatal()
+		}
+	}
+}
+
+func (op StringBuilder) seed() []string {
+	t := make([]string, 1e6)
+	alphabet := op.alphabet()
+	for i := range t {
+		t[i] = op.rand(alphabet)
+	}
+	return t
+}
+
+func BenchmarkOpStringBuilderFirstContinue(b *testing.B) {
+	var op StringBuilder
+	choices := op.seed()
+	for b.Loop() {
+		var s strings.Builder
+		var first = true
+		for _, v := range choices {
+			if first {
+				s.WriteString("> ")
+				s.WriteString(v)
+				s.WriteRune('\n')
+				first = false
+				continue
+			}
+			s.WriteString("  ")
+			s.WriteString(v)
+			s.WriteRune('\n')
+		}
+	}
+}
+
+func BenchmarkOpStringBuilderIndexContinue(b *testing.B) {
+	var op StringBuilder
+	choices := op.seed()
+	for b.Loop() {
+		var s strings.Builder
+		for i, v := range choices {
+			if i == 0 {
+				s.WriteString("> ")
+				s.WriteString(v)
+				s.WriteRune('\n')
+				continue
+			}
+			s.WriteString("  ")
+			s.WriteString(v)
+			s.WriteRune('\n')
+		}
+	}
+}
+
+func BenchmarkOpStringBuilderFirstAlt(b *testing.B) {
+	var op StringBuilder
+	choices := op.seed()
+	for b.Loop() {
+		var s strings.Builder
+		first := true
+		for _, v := range choices {
+			if first {
+				s.WriteRune('>')
+				first = false
+			} else {
+				s.WriteRune(' ')
+			}
+			s.WriteRune(' ')
+			s.WriteString(v)
+			s.WriteRune('\n')
+		}
+	}
+}
+func BenchmarkOpStringBuilderIndexAlt(b *testing.B) {
+	var op StringBuilder
+	choices := op.seed()
+	for b.Loop() {
+		var s strings.Builder
+		for i, v := range choices {
+			if i == 0 {
+				s.WriteRune('>')
+			} else {
+				s.WriteRune(' ')
+			}
+			s.WriteRune(' ')
+			s.WriteString(v)
+			s.WriteRune('\n')
 		}
 	}
 }
